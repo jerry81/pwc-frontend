7bfb0d010b57a6c0ad453f132b1eeb526ab0c026
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-14 13:05:32 +0800

    feat: add axios

--- pwc-fe/src/main.js
+++ pwc-fe/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'http://localhost:3000/'
+    })
+  }
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
