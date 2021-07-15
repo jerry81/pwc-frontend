5dc9aa1395b146a88f7e045777efa360792db711
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-15 12:16:56 +0800

    minor: add prod env back

--- pwc-fe/src/main.js
+++ pwc-fe/src/main.js
@@ -5,7 +5,10 @@ import api from './api'
 
 Vue.config.productionTip = false
 
-axios.defaults.baseURL = 'http://localhost:3000'
+/* read .env for env */
+axios.defaults.baseURL = 'http://localhost:3000' // dev
+// axios.defaults.baseURL = 'http://47.100.10.198:3005'
+
 Object.defineProperty(Vue.prototype, '$api', { value: api })
 
 new Vue({
